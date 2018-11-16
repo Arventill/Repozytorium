7bf3242ed67d1ac993d8833e687df6b14b6a4d07
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2018-11-16 22:28:43 +0100

    Add files via upload

---
+++ sortowanie_bąbelkowe.cpp
@@ -0,0 +1,55 @@
+﻿// sortowanie_bąbelkowe.cpp : 
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+const int ELEMENTS = 4;
+int t[ELEMENTS];
+
+void fill_tab()
+{
+	using namespace std;
+	ifstream plik("dane.txt");
+	if (!plik)
+	{
+		cout << "404\tBrak dostępu do pliku" << endl;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < ELEMENTS; i++)
+		{
+			plik >> t[i];
+		}
+	}
+}
+
+void show_tab()
+{
+	using namespace std;
+	for (int i = 0; i < ELEMENTS; i++)
+	{
+		cout << t[i] << endl;
+	}
+}
+
+int main()
+{
+	using namespace std;
+	int counting = 0;
+
+	fill_tab();
+	do
+	{
+		for (int i = 0; i < ELEMENTS - 1; i++)
+		{
+			if (t[i] > t[i + 1])
+				swap(t[i], t[i + 1]);
+			counting++;
+		}
+	} while (counting != ELEMENTS);
+	show_tab();
+
+	return 0;
+}
---
+++ sortowanie_wstawianie.cpp
@@ -0,0 +1,89 @@
+﻿// sortowanie_wstawianie.cpp : 
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+const int ELEMENTS = 10;
+int t[ELEMENTS];
+
+void fill_tab();
+void show_tab();
+
+int main()
+{
+	using std::swap;
+
+	fill_tab();
+
+	int counting = 0;
+	int iteration;
+	do
+	{
+		int temp = 100;
+		for (int i = counting; i < ELEMENTS; i++)
+		{
+			if (t[i] < temp)
+			{
+				temp = t[i];
+				iteration = i;
+			}
+		}
+		if (t[iteration] < t[counting])
+		{
+			swap(t[iteration], t[counting]);
+			counting++;
+			iteration = 0;
+		}
+		else
+		{
+			counting++;
+			iteration = 0;
+		}
+	} while (counting != ELEMENTS);
+	
+	show_tab();
+	return 0;
+}
+
+void fill_tab()
+{
+	using std::cout;
+	using std::endl;
+	using std::ifstream;
+
+	ifstream plik("dane.txt");
+	if (!plik)
+	{
+		cout << "404\tbrak dostępu do pliku" << endl;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < ELEMENTS; i++)
+		{
+			plik >> t[i];
+		}
+	}
+}
+
+void show_tab()
+{
+	using std::cout;
+	using std::endl;
+	using std::ifstream;
+
+	ifstream plik("dane.txt");
+	if (!plik)
+	{
+		cout << "404\tbrak dostępu do pliku" << endl;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < ELEMENTS; i++)
+		{
+			cout << t[i] << endl;
+		}
+	}
+}
