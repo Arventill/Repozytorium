1f058a2f44ee7ebe68753cb3c687987c59b28798
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2018-11-26 14:37:21 +0100

    Add files via upload

---
+++ Podstawy programowania 3.cpp
@@ -0,0 +1,20 @@
+// Podstawy programowania 3.cpp : Ulamek (10) --> ulamek (2)
+
+#include "pch.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+double dec2bin(double, double); //funkcja przerabiaj¹ca u³amek dziesiêtny na binarny (DEKLARACJA)
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
+double dec2bin(double a, double b) // FUNKCJA
+{
+
+}
