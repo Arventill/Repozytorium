2db36de1ba00bdcfdc927f2c7f735baa400870ce
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2018-11-30 19:45:54 +0100

    Add files via upload

--- Podstawy programowania 3.cpp
+++ Podstawy programowania 3.cpp
@@ -2,19 +2,67 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-double dec2bin(double, double); //funkcja przerabiaj¹ca u³amek dziesiêtny na binarny (DEKLARACJA)
+string total_dec2bin(int);
+string fraction_dec2bin(int);
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	int argA, argB;
+	char oper;
+	string a, b;
+
+	cout << "Podaj liczbe w ulanku dziesietnym: "; cin >> argA >> oper >> argB;
+	a = total_dec2bin(argA);
+	b = fraction_dec2bin(argB);
+
+	cout << endl << "Twoj ulamek to binarnie: " << a << "," << b << endl;
+	system("pause");
+	return 0;
 }
 
-double dec2bin(double a, double b) // FUNKCJA
+string total_dec2bin(int i)
 {
-
+	string wynik = "";
+	while (i)
+	{
+		if (i % 2 == 0)
+		{
+			wynik = "0" + wynik;
+			i /= 2;
+		}
+		else
+		{
+			wynik = "1" + wynik;
+			i /= 2;
+		}
+	}
+	return wynik;
+}
+string fraction_dec2bin(int i)
+{
+	double ii = (double)i;
+	string w = "";
+	while (ii >= 1)
+		ii /= 10;
+	while (ii)
+	{
+		if (ii * 2 < 1)
+		{
+			w += "0";
+			ii = ii * 2;
+		}
+		else
+		{
+			w += "1";
+			ii = ii * 2 - 1;
+		}
+	}
+	return w;
 }
