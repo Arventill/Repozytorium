edd1b60d5a376014124b298a70c634de9d62dcd7
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2018-12-17 14:49:53 +0100

    Add files via upload

---
+++ Podstawy programowania 9.cpp
@@ -0,0 +1,11 @@
+// Podstawy programowania 9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "HAAAA! GEEEEEEEEEEEEEEEEJ\n\n";
+	system("pause");
+	return 0;
+}
